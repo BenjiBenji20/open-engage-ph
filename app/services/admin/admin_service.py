cfc19eea18b3365e27f30010f9bbf2ca30517326
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-09-03 09:35:12 +0800

    -created separate registration service for different user model

---
+++ app/services/admin/admin_service.py
@@ -0,0 +1,46 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.exceptions.customed_exceptions import DuplicateEntryException, InternalServerError
+from app.schemas.admin.admin_schema import AdminRequestSchema, AdminResponseSchema
+from app.models.admin.admin import Admin
+from app.utils.db_look_up_utils import get_user_by_email
+from app.utils.user_validation_utils import hash_password
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+async def admin_registration_service(
+  user: AdminRequestSchema, 
+  db: AsyncSession
+) -> AdminResponseSchema:
+  "handles end user registration logic"
+  try:
+    # validate user by search to its email
+    user_in_db = await get_user_by_email(
+      model=Admin,
+      email=user.email,
+      db=db
+    )
+    
+    # if user already existed, invalidate the registration
+    if user_in_db:
+      raise DuplicateEntryException(f"User with {user.email} as email already exists.")
+    
+    hashed_pw = hash_password(user.password)
+    
+    user_dict = user.model_dump(exclude={"password"})  
+    user_dict["password_hash"] = hashed_pw 
+
+    user_db = Admin(**user_dict) 
+    db.add(user_db)
+    await db.commit()
+    await db.refresh(user_db)
+
+    return user_db
+  except DuplicateEntryException:
+    raise 
+  except Exception as e:
+    logger.error(f"An error occured: {e}")
+    raise InternalServerError("An expected error occured.")
+  
